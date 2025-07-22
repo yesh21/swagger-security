5b3fb901e4927b1d14180c4cb3500c21385dab06
yesh21 <yaswanth.pulavarthi@gmail.com>
2025-07-22 22:11:56 +0530

    added secret parameter middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+      app.use(['/docs', '/docs-json'], (req: Request, res: Response, next: NextFunction) => {
+  if (req.path === '/' || req.path === '') {
+    if (req.query.secret !== 'abrakadabra-useinenvfiles') {
+      return res.redirect('/');
+    }
+  }
+  next();
+});
     const config = new DocumentBuilder()
     .setTitle('My API')
     .setDescription('description') 
